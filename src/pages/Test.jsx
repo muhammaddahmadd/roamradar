b5efabaad3d4832bbb671ff0a4fac81d19697c28
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-02-21 21:32:08 +0500

    Working

---
+++ src/pages/Test.jsx
@@ -0,0 +1,13 @@
+
+import PageNav from "../components/PageNav"
+function Test() {
+    return (
+        <div>
+            <PageNav />
+            <h2>Hi, this is just for testing</h2>
+           
+        </div>
+    )
+}
+
+export default Test
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { ESLint } from 'eslint'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), ESLint()],
+})
