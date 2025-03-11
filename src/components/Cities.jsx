782f16a9a0490c0be72c902b0a2b62209a821707
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-03-11 22:22:04 +0500

    ready for production

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,22 +1,21 @@
 import PropTypes from "prop-types";
-import Spinner from "./Spinner"
+import Spinner from "./Spinner";
 import CityItem from "./CityItem";
-import styles from './Cities.module.css'
-import Message from "./Message"
+import styles from "./Cities.module.css";
+import Message from "./Message";
 import { useCities } from "../contexts/CitiesContext";
 const Cities = () => {
-  const {isLoading, cities} = useCities()
+  const { isLoading, cities } = useCities();
 
-  console.log(cities)
-
-
-
-    if(isLoading) return  <Spinner />
-    if (!cities.length) return <Message message={"Please start adding cities to your list"}/>
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message={"Please start adding cities to your list"} />;
 
   return (
     <ul className={styles.cityList}>
-       { cities.map((city) => <CityItem key={city.id} city={city} />)}
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
     </ul>
   );
 };
