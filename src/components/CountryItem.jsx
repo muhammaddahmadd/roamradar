5f3ab8ee1aeae67e474b5d79750cc5165fc027e5
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-03-12 21:11:48 +0500

    wokring fine

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,12 +1,6 @@
 import styles from "./CountryItem.module.css";
 
 function CountryItem({ emoji, country }) {
-
-  
-  
-  // const unique_countries = new Set([country])
-  // console.log(unique_countries)
-
   return (
     <li className={styles.countryItem}>
       <span>{emoji}</span>
