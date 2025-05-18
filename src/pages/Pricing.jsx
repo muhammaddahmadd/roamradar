096cb83a1fcd30dee36da2b550759a2273db9484
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-05-18 23:35:05 +0500

    Update Pricing component: replace placeholder text with simplified dummy text

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -13,9 +13,7 @@ export default function Product() {
             Just $9/month.
           </h2>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
-            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
-            voluptatem iste.
+            Some dummy text..
           </p>
         </div>
         <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
