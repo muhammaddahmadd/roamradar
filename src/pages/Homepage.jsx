4110c8c4d5f544a19bab36aa7d5b19098027abaa
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-05-25 00:03:55 +0500

    Enhance Homepage description: add a personal touch to the text about tracking experiences

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -14,7 +14,8 @@ export default function Homepage() {
         </h1>
         <h2>
           A world map that tracks your footsteps into every city you can think
-          of.
+          of. Never forget your wonderful experiences and show your friends how
+          you have wandered the world.
         </h2>
         <Link to="/login" className="cta">
           Start Tracking Now..
