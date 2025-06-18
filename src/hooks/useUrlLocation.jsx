11dc5202c89d86ab0531ee5ed4a9c19398b908f9
Muhammad Ahmad <133768693+muhammaddahmadd@users.noreply.github.com>
2025-06-18 21:21:38 +0500

    Update useUrlLocation.jsx

--- src/hooks/useUrlLocation.jsx
+++ src/hooks/useUrlLocation.jsx
@@ -1,7 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 
 export default function useUrlLocation() {
-
       const [searchParams] = useSearchParams();
       const lat = searchParams.get("lat");
       const lng = searchParams.get("lng");
