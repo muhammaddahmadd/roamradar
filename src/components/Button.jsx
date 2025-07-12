c76318232677d2352ad53337cda55b087b4bbfd7
Muhammad Ahmad <133768693+muhammaddahmadd@users.noreply.github.com>
2025-07-12 23:55:21 +0500

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,6 @@
 import styles from './Button.module.css'
 function Button({type, onClick, children}) {
+    
     return (
         <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
             {children}
