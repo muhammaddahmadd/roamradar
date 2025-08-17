b35d7f5aa57108a8a6b9f53cef3162cad62af9e3
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-08-18 04:17:40 +0500

    build fixes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/useAuth";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import SignUp from "./pages/SignUp";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -24,7 +24,7 @@ function App() {
             <Route path="/" element={<Homepage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
-            <Route path="signup" element={<SignUp />} />
+            <Route path="signup" element={<Signup />} />
             <Route
               path="app"
               element={
