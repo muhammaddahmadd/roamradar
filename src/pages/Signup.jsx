e25127f7a7621dadf3dbc69600d00f9f9c0f2981
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-08-18 04:26:42 +0500

    nit fixes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -14,6 +14,7 @@ const Signup = () => {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
